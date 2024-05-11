d6827f2a47a90c0571c05d12464f91c210436e68
mastermindmayank <88104889+mastermindmayankofficial@users.noreply.github.com>
2024-05-11 22:08:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Mastermind")
+DATABASE_NAME = environ.get('DATABASE_NAME', "MASTERMIND")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'not_anime_search_bot')
 
 # Premium And Referal Settings
